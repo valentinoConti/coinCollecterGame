9994d02bbeda5c633fd7b3e8abc865fdd5bfa7ec
Valentino Conti <41705567+valentinoConti@users.noreply.github.com>
2018-08-15 12:34:59 -0300

    Insertable || Preload
    Removed 'Event Catcher' div, all the events are detected with the canvas element itself;
    
    All the images and sounds are now loaded with javascript and not on the HTML document;
    
    The game now will wait until all the images and sounds are loaded to start printing things;
    
    Now the game can be inserted on any web (before, the game had to be on the upper-left corner of the page to work properly).

--- index.js
+++ index.js
@@ -2,6 +2,11 @@
 var gameInterval,
  canv,
  ctx,
+ images = [],
+ sounds = [],
+ soundsLoaded = [],
+ imagesLoaded = false,
+ soundsLoaded = false,
  heroX = 150,
  heroY = 115,
  coinX = 150,
@@ -36,16 +41,16 @@ function drawTheHero(g) {
 }
 
 function drawEnemy(g) {
-    g.drawImage(enemy, enemyX, enemyY, 30, 30);
+    g.drawImage(images[1], enemyX, enemyY, 30, 30);
     if (points <= 249) {
-        g.drawImage(jaula, 0, 0, 50, 50);
+        g.drawImage(images[6], 0, 0, 50, 50);
         enemyX = 10;
         enemyY = 10;
         if (points >= 248) {
             if (soundOn) {
-                tacs.pause();
-                tacs.currentTime = 0;
-                tacs.play();
+                sounds[2].pause();
+                sounds[2].currentTime = 0;
+                sounds[2].play();
             }
         }
     } else {
@@ -67,7 +72,7 @@ function drawEnemy(g) {
 }
 
 function drawCoin(g) {
-    g.drawImage(img4, coinX, coinY, 30, 30);
+    g.drawImage(images[4], coinX, coinY, 30, 30);
 }
 
 function drawLine(g, startX, startY, endX, endY) {
@@ -79,64 +84,64 @@ function drawLine(g, startX, startY, endX, endY) {
 
 function drawSounds() {
     if (soundOn && musicOn) {
-        ctx.drawImage(soundIcon, 530, 50, 40, 40);
-        ctx.drawImage(musicIcon, 480, 50, 40, 40);
+        ctx.drawImage(images[11], 530, 50, 40, 40);
+        ctx.drawImage(images[12], 480, 50, 40, 40);
     }else if (soundOn && !musicOn) {
-        ctx.drawImage(soundIcon, 530, 50, 40, 40);
-        ctx.drawImage(musicIcon, 480, 50, 40, 40);
-        ctx.drawImage(errorIcon, 480, 50, 40, 40);
+        ctx.drawImage(images[11], 530, 50, 40, 40);
+        ctx.drawImage(images[12], 480, 50, 40, 40);
+        ctx.drawImage(images[13], 480, 50, 40, 40);
     }else if (!soundOn && musicOn) {
-        ctx.drawImage(soundIcon, 530, 50, 40, 40);
-        ctx.drawImage(musicIcon, 480, 50, 40, 40);
-        ctx.drawImage(errorIcon, 530, 50, 40, 40);
+        ctx.drawImage(images[11], 530, 50, 40, 40);
+        ctx.drawImage(images[12], 480, 50, 40, 40);
+        ctx.drawImage(images[13], 530, 50, 40, 40);
     }else{
-        ctx.drawImage(soundIcon, 530, 50, 40, 40);
-        ctx.drawImage(musicIcon, 480, 50, 40, 40);
-        ctx.drawImage(errorIcon, 530, 50, 40, 40);
-        ctx.drawImage(errorIcon, 480, 50, 40, 40);
+        ctx.drawImage(images[11], 530, 50, 40, 40);
+        ctx.drawImage(images[12], 480, 50, 40, 40);
+        ctx.drawImage(images[13], 530, 50, 40, 40);
+        ctx.drawImage(images[13], 480, 50, 40, 40);
     }
 }
 
 function checkDiff() {
     if (points >= 0 && points < 25) { 
         texto = 'Beginner';
-        apuro.playbackRate = 1;
+        sounds[5].playbackRate = 1;
         return "#c8cbd1";
     } else
     if (points >= 25 && points < 50) {
-    	return "#b6bbc4";
+        return "#b6bbc4";
     } else
     if (points >= 50 && points < 75) {
-    	return "#9da3ad";
+        return "#9da3ad";
     } else
     if (points >= 75 && points < 100) {
         if (points == 75) dificultad = 7;
         return "#8e95a0";
     } else
     if (points >= 100 && points < 150) {
-    	return "#78808c";
+        return "#78808c";
     } else
     if (points >= 150 && points < 200) { 
-    	return "#6b7482";
+        return "#6b7482";
     } else
     if (points >= 200 && points < 250) {
         texto = 'Mouse enthusiast';
-    	return "#535e70"; 
+        return "#535e70"; 
     } else
     if (points >= 250 && points < 300) {
         if (points == 250) dificultad = 8;
-    	return "#475770";
+        return "#475770";
     } else
     if (points >= 300 && points < 350) {
-    	return "#3a4c68";
+        return "#3a4c68";
     } else
     if (points >= 350 && points < 400) {
-    	return "#2c3e5b";
+        return "#2c3e5b";
     }
     if (points >= 400 && points < 500) {
         texto = 'Mouse addict';
-        apuro.playbackRate = 1.2
-    	return "#233451"; 
+        sounds[5].playbackRate = 1.2
+        return "#233451"; 
     } else
     if (points >= 500 && points < 600) {
         if (rapido) {
@@ -144,51 +149,51 @@ function checkDiff() {
             enemySpeedX = enemySpeedX*1.4;
             enemySpeedY = enemySpeedY*1.4;
         }
-        apuro.playbackRate = 1.4
-    	return "#192a47";
+        sounds[5].playbackRate = 1.4
+        return "#192a47";
     } else
     if (points >= 600 && points < 700) {
         rapido = true;
         texto = 'Mouse professional';
-        apuro.playbackRate = 1.6
-    	return "#122442"; 
+        sounds[5].playbackRate = 1.6
+        return "#122442"; 
     } else
     if (points >= 700 && points < 750) {
-    	if (rapido) {
+        if (rapido) {
             rapido = false;
             dificultad = 9; 
             enemySpeedX = enemySpeedX*1.4; 
             enemySpeedY = enemySpeedY*1.4; 
         }
-    	return "#0a1d3a";
+        return "#0a1d3a";
     } else
     if (points >= 750 && points < 800) {
-        apuro.playbackRate = 1.8
-    	return "#0a1d3a";
+        sounds[5].playbackRate = 1.8
+        return "#0a1d3a";
     } else
     if (points >= 800 && points < 850) {
         texto = 'You are a legend';
-    	return "#021430"; 
+        return "#021430"; 
     } else
     if (points >= 850 && points < 900) {
         if (points == 850) dificultad = 10;
-        apuro.playbackRate = 2
-    	return "#021430";  
+        sounds[5].playbackRate = 2
+        return "#021430";  
     } else
     if (points >= 900 && points < 1000){
         if (points == 915) dificultad = 11;
         texto = 'SAVAGE. You just won';
-    	return "#ff33cc";  
+        return "#ff33cc";  
     } else {
-    	return "#f442b9";
+        return "#f442b9";
     }
 }
 
 function drawClock(g) {
     if (clocks <= 8) { 
-        g.drawImage(img2, clockX, clockY, 30, 30); 
+        g.drawImage(images[2], clockX, clockY, 30, 30); 
     } else {
-        g.drawImage(img5, clockX, clockY, 40, 40);
+        g.drawImage(images[5], clockX, clockY, 40, 40);
     }
         
 }
@@ -207,14 +212,14 @@ function isTouchingClock() {
 
 function moveClock() {
     if (clocks <= 8) {
-        if (soundOn) campan.play();
+        if (soundOn) sounds[3].play();
         clockX = (Math.random() * (canv.width-60))+30;
         clockY = (Math.random() * (canv.height-60))+30;
         clocks++;
         timer += 3;
         reloj = 0;
     } else {
-        if (soundOn) paf.play();
+        if (soundOn) sounds[1].play();
         clockX = (Math.random() * (canv.width-60))+30;
         clockY = (Math.random() * (canv.height-60))+30;
         clocks = 0;
@@ -226,19 +231,19 @@ function moveClock() {
 function caraa() {   //changing player function
     switch(cara) {
         case 1:
-            img = document.getElementById("carita");
+            img = images[14];
             break;
         case 2:
-            img = document.getElementById("carita2");
+            img = images[15];
             break;
         case 3:
-            img = document.getElementById("carita3");
+            img = images[16];
             break;
         case 4:
-            img = document.getElementById("carita4");
+            img = images[17];
             break;
         case 5:
-        	img = document.getElementById("carita5");
+            img = images[18];
         }
 }
 
@@ -265,16 +270,24 @@ function moveCoin() {
     reloj++;
     yaTa = false;
     if (soundOn) {
-        pium.pause();
-        pium.currentTime = 0;
-        pium.play();
+        sounds[0].pause();
+        sounds[0].currentTime = 0;
+        sounds[0].play();
     }
 }
 
 //Events Functions
 function canvasMove(E) {  //Get mouse X,Y
-    heroX = E.pageX;
-    heroY = E.pageY;
+    var rect = canv.getBoundingClientRect();
+    heroX = E.clientX - rect.left;
+    heroY = E.clientY - rect.top;
+    if (!jugando && inTheMenu) {
+        if (E.pageX >= 431 && E.pageX <= 600 && E.pageY >= 212 && E.pageY <= 280) {
+            canv.style.cursor = 'pointer';
+        } else {
+            canv.style.cursor = 'default';
+        }
+    }
 }
 
 function clickeado(E) {  //Click for return to the startScreen.
@@ -326,7 +339,7 @@ function keyPush(E) {   //Keyboard input
 
     if (E.keyCode == 13) {   //Key ENTER: StartPlaying
         if (!jugando) {
-        	if (cara != 5) eventDiv.style.cursor = 'none';
+            if (cara != 5) canv.style.cursor = 'none';
             points = 0;
             inTheMenu = false;
             gameInterval = requestAnimationFrame(runGame);
@@ -338,58 +351,96 @@ function keyPush(E) {   //Keyboard input
         if (stop) {
             points++;
             if (soundOn) {
-                pium.pause();
-                pium.currentTime = 0;
-                pium.play();
+                sounds[0].pause();
+                sounds[0].currentTime = 0;
+                sounds[0].play();
             }
         }
     }
 }   
 
-//Getting everything ready, parsing everything
+
 function startLoading() {
+    //Getting everything ready, parsing everything
 
-    imgTitle = document.getElementById("collecter");
+    function preloadIMG() {
+        var i,
+        c = 0,
+        totalito = preloadIMG.arguments.length;
+        for (i = 0; i < totalito; i++) {
+            images[i] = new Image();
+            images[i].onload = function(){
+                c++;
+                if (c == totalito) {
+                    imagesLoaded = true;
+                }
+            }
+            images[i].src = preloadIMG.arguments[i];
+        }
+    }
+
+    function preloadAUD() {
+        var i, 
+        d = 0, 
+        totalito = preloadAUD.arguments.length;
+        for (i = 0; i < totalito; i++) {
+            sounds[i] = document.createElement('audio');
+            sounds[i].oncanplay = function(){
+                d++;
+                if (d == totalito) {
+                    soundsLoaded = true;
+                }
+            }
+            sounds[i].src = preloadAUD.arguments[i];
+        }
+    }
+
+    preloadIMG(
+        'images\\coinCollecter.png',
+        'images\\chau.png',
+        'images\\reloj.png',
+        'images\\mousee.png',
+        'images\\moneda.png',
+        'images\\reloj2.png',
+        'images\\jail.png',
+        'images\\gitLogo.png',
+        'images\\spacebar.png',
+        'images\\register.png',
+        'images\\charac.png',
+        'images\\sound.png',
+        'images\\music.png',
+        'images\\error.png',
+        'images\\hola.png',
+        'images\\holaa.png',
+        'images\\holaaa.png',
+        'images\\holaaaa.png',
+        'images\\holis.png'
+    );
     caraa();
-    enemy = document.getElementById("enemigo");
-    img2 = document.getElementById("relojito");
-    img3 = document.getElementById("mousito");
-    img4 = document.getElementById("monedita");
-    img5 = document.getElementById("reloj2");
-    jaula = document.getElementById("jail");
-    yo = document.getElementById("logo");
-    space = document.getElementById("spacebar");
-    registrarse = document.getElementById("register");
-    personajes = document.getElementById("charac");
-    soundIcon = document.getElementById("soundIco");
-    musicIcon = document.getElementById("musicIco");
-    errorIcon = document.getElementById("errorIco");
-    pium = document.getElementById("piuf");
-    paf = document.getElementById("paaf");
-    tacs = document.getElementById("taas");
-    campan = document.getElementById("campana");
-    campan.volume = 0.5;
-    fin = document.getElementById("final");
-    fin.volume = 0.5;
-    apuro = document.getElementById("apurar");
-    apuro.volume = 0.1;
-    canv = document.getElementById("GraphicsBox");
-    ctx = canv.getContext("2d");
-    //First clock position
-    clockX = (Math.random() * (canv.width-60))+30;
-    clockY = (Math.random() * (canv.height-60))+30;
-    //Event Catcher
-    eventDiv = document.getElementById("EventCatcher");
-    eventDiv.addEventListener("mousemove", canvasMove);
-    eventDiv.addEventListener("click", clickeado);
-    document.addEventListener("keydown", keyPush);
-    gameInterval = setInterval(hasLoaded, 250);
+    preloadAUD(
+        'sound\\piuf.mp3',
+        'sound\\paaf.mp3',
+        'sound\\TAAS.mp3',
+        'sound\\campana.mp3',
+        'sound\\final.mp3',
+        'sound\\hurryUpViolin.mp3'
+    );
+    gameInterval = setInterval(hasLoaded, 25);   
 }
-
-function hasLoaded()
-{
-    if (true) // Check to see if all info is loaded
-    {
+function hasLoaded(){
+    if (soundsLoaded && imagesLoaded) {
+        sounds[3].volume = 0.5;
+        sounds[4].volume = 0.5;
+        sounds[5].volume = 0.1;
+        canv = document.getElementById("GraphicsBox");
+        ctx = canv.getContext("2d");
+        //First clock position
+        clockX = (Math.random() * (canv.width-60))+30;
+        clockY = (Math.random() * (canv.height-60))+30;
+        //Event Catcher
+        canv.addEventListener("mousemove", canvasMove);
+        canv.addEventListener("click", clickeado);
+        document.addEventListener("keydown", keyPush);
         clearInterval(gameInterval);
         runStart();
     }
@@ -397,15 +448,15 @@ function hasLoaded()
 
 //Start screen
 function runStart() {
-	eventDiv.style.cursor = 'default';
+    canv.style.cursor = 'default';
     inTheMenu = true;
     ctx.fillStyle = "#9da3ad";
     ctx.fillRect(0,0,canv.width,canv.height);
     ctx.drawImage(img, 150, 115, 30, 30);
     drawCoin(ctx); 
-    ctx.drawImage(img2, 150, 200, 30, 30);
-    ctx.drawImage(img5, 150, 240, 30, 30);
-    ctx.drawImage(enemy, 150, 75, 30, 30);
+    ctx.drawImage(images[2], 150, 200, 30, 30);
+    ctx.drawImage(images[5], 150, 240, 30, 30);
+    ctx.drawImage(images[1], 150, 75, 30, 30);
     drawText(ctx, 'You are this guy', 190, 135, 22);
     drawText(ctx, 'This guy is your enemy', 190, 95, 22);
     drawText(ctx, 'Collect these coins', 190, 180, 22);
@@ -421,10 +472,10 @@ function runStart() {
     ctx.fillRect(0, 281, 600, 119)
     drawText(ctx, 'You will have to be fast with the mouse!', 195, 350, 25);
     drawText(ctx, 'Press Enter to play!', 405, 385, 25);
-    ctx.drawImage(img3, 10, 250, 150, 150);
-    ctx.drawImage(imgTitle, 150, 10, 260, 100);
+    ctx.drawImage(images[3], 10, 250, 150, 150);
+    ctx.drawImage(images[0], 150, 10, 260, 100);
     drawSounds();
-    ctx.drawImage(yo, 431, 212, 169, 68);
+    ctx.drawImage(images[7], 431, 212, 169, 68);
     drawLine(ctx, 430, 212, 600, 212);
     drawLine(ctx, 430, 100, 600, 100);
     drawLine(ctx, 430, 150, 600, 150);
@@ -440,7 +491,7 @@ function update() {
     ctx.fillStyle = checkDiff();
     ctx.fillRect(0,0,canv.width,canv.height);  //Background Screen
     cont++;
-    if (musicOn) apuro.play(); else apuro.pause();
+    if (musicOn) sounds[5].play(); else sounds[5].pause();
 
     if (!stop) {  //jugando
         if (!yaTa && (points == 110 || points == 211 || points == 315 || points == 433 || points == 651 || points == 820 || points == 922)) {
@@ -448,7 +499,7 @@ function update() {
             stop = true;
         }
         if (isTouchingCoin()) moveCoin();
-        if (isTouchingEnemy() && points >= 360) timer = 0;
+        if (isTouchingEnemy() && points >= 251) timer = 0;
         
         if (cont >= 60) {
             timer--;
@@ -476,15 +527,15 @@ function draw() {
             if (isTouchingClock()) moveClock();
         }
     } else {
-        ctx.drawImage(space, 240, 360, 120, 30);
+        ctx.drawImage(images[8], 240, 360, 120, 30);
         drawText(ctx, 'ASD ASD ASD ASD!!!', 185, 345, 30);
     } 
 }
 
 function lose() {
     canv.style.cursor = "default";
-    apuro.pause();
-    if (soundOn) fin.play();
+    sounds[5].pause();
+    if (soundOn) sounds[4].play();
     window.cancelAnimationFrame(gameInterval);
     runEnd();
 }
@@ -492,12 +543,12 @@ function lose() {
 //Playing the game
 function runGame() 
 {
-	now = window.performance.now();
-  	dt = dt + Math.min(1, (now - last) / 1000);
-  	if(dt > step) {      //making sure logic goes 60fps
-    	dt = dt - step;
+    now = window.performance.now();
+    dt = dt + Math.min(1, (now - last) / 1000);
+    if(dt > step) {      //making sure logic goes 60fps
+        dt = dt - step;
         update();
-	}
+    }
     draw();             //but drawing as fast as it cans
     last = now;
     gameInterval = requestAnimationFrame(runGame);
@@ -520,8 +571,8 @@ function runEnd()
     drawLine(ctx, 0, 65, 600, 65);
     drawText(ctx, 'Click for the Menu', 435, 280, 20);
     drawText(ctx, 'Press ENTER to Play again!', 7, 390, 20);
-    ctx.drawImage(img3, 440, 250, 150, 150);
-    ctx.drawImage(charac, 6, 80, 250, 78);
+    ctx.drawImage(images[3], 440, 250, 150, 150);
+    ctx.drawImage(images[10], 6, 80, 250, 78);
     drawText(ctx, ': ' + texto, 266, 130, 30);
     jugando = false;
     cont = 0;
@@ -530,6 +581,6 @@ function runEnd()
     reloj = 0;
     rapido = true;
     dificultad = 6;
-	enemySpeedX = 2;
-	enemySpeedY = 2;
+    enemySpeedX = 2;
+    enemySpeedY = 2;
 }
